14194bd0d9458fd5036b113d417a93c7d4d05875
hongwongi <h>
2022-06-29 21:30:22 +0900

    Initial Commit

---
+++ GTest/GTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GTestApp.swift
+//  GTest
+//
+//  Created by MacBook Air on 2022/06/29.
+//
+
+import SwiftUI
+
+@main
+struct GTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
